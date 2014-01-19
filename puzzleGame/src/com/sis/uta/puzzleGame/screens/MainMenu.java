f38dc42b8efac3c5bd5e3a006152927432878575
elefantino <net.tyumen@mail.ru>
2014-01-19 20:53:23 +0200

    music

--- puzzleGame/src/com/sis/uta/puzzleGame/screens/MainMenu.java
+++ puzzleGame/src/com/sis/uta/puzzleGame/screens/MainMenu.java
@@ -160,7 +160,7 @@ public class MainMenu implements Screen {
 		{
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
-				puzzleAudio.stopMusic();
+				//puzzleAudio.stopMusic();
 			}
 		});
 		
